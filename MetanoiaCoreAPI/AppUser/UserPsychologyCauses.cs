40883aca888f1e8a8f90d724b5c236f3ffaa4c72
PhantomK520 <InitK.enterprise@gmail.com>
2020-09-14 15:13:52 +0630

    Initial Commit

---
+++ MetanoiaCoreAPI/AppUser/UserPsychologyCauses.cs
@@ -0,0 +1,31 @@
+namespace MetanoiaCoreAPI.AppUser
+
+{
+
+    public class UserPsychologyCauses
+    {
+        public int ID {get;set;}
+
+        public bool RelationshipProblem {get; set;}
+
+        public bool FamilyConflict {get; set;}
+
+        public bool LossingSomeone {get;set;}
+
+        public bool Rape {get;set;}
+
+        public bool SexualAbuse {get;set;}
+
+        public bool WorkProblem {get;set;}
+
+        public bool ClingtoSomething {get; set;}
+
+     
+
+
+    }
+
+
+
+
+}
---
+++ MetanoiaCoreAPI/AppUser/UserPsychologyEffects.cs
@@ -0,0 +1,42 @@
+namespace MetanoiaCoreAPI.AppUser
+{
+        public class UserPsychologyEffects
+    {
+        public int ID {get;set;}
+
+        
+
+        public bool SleepProblem {get;set;}
+
+        public bool LossofAppetite {get; set;}
+
+        public bool WeightLossORWeightGain {get; set;}
+
+        public bool FocusProblem {get; set;}
+
+        public bool AngerProblem {get; set;}
+        
+        public bool ConstantWorry {get;set;}
+
+        public bool LonelinessORIsolation {get; set;}
+
+        public bool FeelingOverWhelmed {get; set;}
+
+        public bool Unhappiness {get; set;}
+
+        public bool SuicidalThoughts {get;set;}
+
+        public bool NoJoy {get; set;}
+
+        public bool FeelingSadORDown {get;set;}
+
+        public bool OveruseOfAlcholAndDrugs {get;set;}
+
+        public bool WithdrawFromFriendsORActivities {get; set;}
+
+        public bool SexDriveChange {get;set;}
+
+        public bool MoodSwing {get; set;}
+
+    }
+}
